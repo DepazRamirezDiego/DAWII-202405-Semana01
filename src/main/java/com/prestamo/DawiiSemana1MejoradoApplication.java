4294937454cb6bb5e2e7fd0762071ec774c56b3a
Diego Depaz Ramirez <depazramirezdiego02@gmail.com>
2024-09-25 10:12:05 -0500

    Primer commit

---
+++ src/main/java/com/prestamo/DawiiSemana1MejoradoApplication.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiSemana1MejoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiSemana1MejoradoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/prestamo/repository/PaisRepository.java
@@ -0,0 +1,14 @@
+package com.prestamo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prestamo.entity.Pais;
+
+public interface PaisRepository extends JpaRepository<Pais, Integer>{
+
+
+
+	
+}
